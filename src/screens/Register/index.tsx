d82401fe4b4ad576e36a8839546e3753a59eee4a
j-eugenio <eugeniopb4@gmail.com>
2021-09-20 17:32:27 -0300

    Input, botoes,

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionsTypes
+} from './styles';
+
+export function Register(){
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+
+          <Input
+            placeholder="Preço"
+          />
+
+          <TransactionsTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+            />
+
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+            />
+          </TransactionsTypes>
+        </Fields>
+
+        <Button title="Enviar"/>
+      </Form>
+    </Container>
+  )
+}
