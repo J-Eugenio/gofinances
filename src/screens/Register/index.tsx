3c4ce942b90f8a4845839ef8f841ec91f3b7ea46
j-eugenio <eugeniopb4@gmail.com>
2021-10-15 13:06:37 -0300

    Salvando novas transações

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Keyboard,
   Modal,
@@ -9,8 +9,10 @@ import {
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
 
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -58,9 +60,12 @@ export function Register(){
     name: 'Categoria'
   });
 
+  const navigation = useNavigation();
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
@@ -88,10 +93,12 @@ export function Register(){
       return Alert.alert('Selecione a categoria')
 
     const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
-      category: category.key
+      category: category.key,
+      date: new Date()
     }
 
     try {
@@ -104,28 +111,22 @@ export function Register(){
       ]
 
       await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted))
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      //@ts-ignore
+      navigation.navigate('Listagem');
+
     } catch (error) {
-      console.log(error)
       Alert.alert('Nao foi possivel salvar')
     }
 
   }
-
-  useEffect(() => {
-    async function loadData(){
-      const data = await AsyncStorage.getItem(dataKey);
-      console.log(JSON.parse(data!));
-
-    }
-
-    loadData();
-
-     /* async function removeAll() {
-      await AsyncStorage.removeItem(dataKey);
-     }
-
-     removeAll() */
-  }, [])
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
